79a1cc9377a86232790bfaabfaa3435d9dfd44e3
Stuart Allen <sallen@wiley.com>
2020-02-07 12:44:44 +0000

    adding userback

---
+++ assets/js/userback.js
@@ -0,0 +1,7 @@
+Userback = window.Userback || {};
+    Userback.access_token = '7984|13248|pqvjrcHd3vlxuBTrI84IVwNiRophBQtuuqcDN5YpwKazwhLPd9';
+    (function(id) {
+        var s = document.createElement('script');
+        s.async = 1;s.src = 'https://static.userback.io/widget/v1.js';
+        var parent_node = document.head || document.body;parent_node.appendChild(s);
+    })('userback-sdk');
